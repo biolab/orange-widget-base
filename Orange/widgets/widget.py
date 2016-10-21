ales-erjavec/orange3

widget: use super in WidgetMetaClass.__new__

Orange/widgets/widget.py
@@ -41,7 +41,7 @@ class WidgetMetaClass(type(QDialog)):
         from Orange.canvas.registry.description import (
             input_channel_from_args, output_channel_from_args)
 
-        cls = type.__new__(mcs, name, bases, dict)
+        cls = super().__new__(mcs, name, bases, dict)
         if not cls.name: # not a widget
             return cls
 

biolab/orange-widget-base

Orange/widgets/widget.py
@@ -47,7 +47,7 @@ class WidgetMetaClass(type(QDialog)):
         from Orange.canvas.registry.description import (
             input_channel_from_args, output_channel_from_args)
 
-        cls = type.__new__(mcs, name, bases, kwargs)
+        cls = super().__new__(mcs, name, bases, kwargs)
         if not cls.name: # not a widget
             return cls
 
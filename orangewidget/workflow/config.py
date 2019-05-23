ffb6a6210e68369e5eec9308f2497c49b230ab1e
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-05-23 17:35:51 +0200

    config: Make the config more general

--- orangewidget/workflow/config.py
+++ orangewidget/workflow/config.py
@@ -5,15 +5,12 @@ Orange Canvas Configuration
 import os
 import sys
 import itertools
-from distutils.version import LooseVersion
-
 from typing import Dict, Any, Optional, Iterable, List
 
 import pkg_resources
 import requests
 
-from AnyQt.QtGui import QPainter, QFont, QFontMetrics, QColor, QPixmap, QIcon
-from AnyQt.QtCore import Qt, QPoint, QRect, QStandardPaths, QCoreApplication
+from AnyQt.QtCore import QStandardPaths, QCoreApplication
 
 from orangecanvas import config
 
@@ -22,12 +19,11 @@ from orangecanvas import config
 OFFICIAL_ADDON_LIST = "https://orange.biolab.si/addons/list"
 
 WIDGETS_ENTRY = "orange.widgets"
-ADDONS_ENTRY = "orange3.addon"
 
 
-class Config(config.Config):
+class Config(config.Default):
     """
-    Orange application config
+    Basic configuration for running orangewidget based workflow application.
     """
     OrganizationDomain = "biolab.si"
     ApplicationName = "Orange Canvas"
@@ -37,60 +33,12 @@ class Config(config.Config):
         ApplicationVersion = "0.0.0"
 
     @staticmethod
-    def application_icon():
-        """
-        Return the main application icon.
-        """
-        path = pkg_resources.resource_filename(
-            __name__, "icons/orange-canvas.svg"
-        )
-        return QIcon(path)
-
-    @staticmethod
-    def splash_screen():
-
-        path = pkg_resources.resource_filename(
-            __name__, "icons/orange-splash-screen.png")
-        pm = QPixmap(path)
-
-        version = Config.ApplicationVersion
-        if version:
-            version_parsed = LooseVersion(version)
-            version_comp = version_parsed.version
-            version = ".".join(map(str, version_comp[:2]))
-        size = 21 if len(version) < 5 else 16
-        font = QFont("Helvetica")
-        font.setPixelSize(size)
-        font.setBold(True)
-        font.setItalic(True)
-        font.setLetterSpacing(QFont.AbsoluteSpacing, 2)
-        metrics = QFontMetrics(font)
-        br = metrics.boundingRect(version).adjusted(-5, 0, 5, 0)
-        br.moveCenter(QPoint(436, 224))
-
-        p = QPainter(pm)
-        p.setRenderHint(QPainter.Antialiasing)
-        p.setRenderHint(QPainter.TextAntialiasing)
-        p.setFont(font)
-        p.setPen(QColor("#231F20"))
-        p.drawText(br, Qt.AlignCenter, version)
-        p.end()
-        return pm, QRect(88, 193, 200, 20)
-
-    @staticmethod
     def widgets_entry_points():
         """
-        Return an `EntryPoint` iterator for all 'orange.widget' entry
-        points plus the default Orange Widgets.
+        Return an `EntryPoint` iterator for all registered 'orange.widgets'
+        entry points.
         """
-        # This could also be achieved by declaring the entry point in
-        # Orange's setup.py, but that would not guaranty this entry point
-        # is the first in a list.
-        dist = pkg_resources.get_distribution("Orange3")
-        default_ep = pkg_resources.EntryPoint(
-            "Orange Widgets", "Orange.widgets", dist=dist)
-        return itertools.chain(
-            (default_ep,), pkg_resources.iter_entry_points(WIDGETS_ENTRY))
+        return pkg_resources.iter_entry_points(WIDGETS_ENTRY)
 
     @staticmethod
     def addon_entry_points():
@@ -117,7 +65,7 @@ class Config(config.Config):
         be updated. The package that defines the application's `main()` entry
         point must always be in this list.
         """
-        return ["Orange3 >=3.20,<4.0a"]
+        return ["orange-widget-base"]
 
     @staticmethod
     def examples_entry_points():
@@ -145,27 +93,6 @@ class Config(config.Config):
     def workflow_constructor(*args, **kwargs):
         from .widgetsscheme import WidgetsScheme
         return WidgetsScheme(*args, **kwargs)
-
-    APPLICATION_URLS = {
-        #: Submit a bug report action in the Help menu
-        "Bug Report": "https://github.com/biolab/orange3/issues",
-        #: A url quick tour/getting started url
-        "Quick Start": "https://orange.biolab.si/getting-started/",
-        #: The 'full' documentation, should be something like current /docs/
-        #: but specific for 'Visual Programing' only
-        "Documentation": "https://orange.biolab.si/toolbox/",
-        #: YouTube tutorials
-        "Screencasts":
-            "https://www.youtube.com/watch"
-            "?v=HXjnDIgGDuI&list=PLmNPvQr9Tf-ZSDLwOzxpvY-HrE0yv-8Fy&index=1",
-        #: Used for 'Submit Feedback' action in the help menu
-        "Feedback": "https://orange.biolab.si/survey/long.html",
-    }
-
-
-def init():
-    # left for backwards compatibility
-    raise RuntimeError("This is not the init you are looking for.")
 
 
 def data_dir_base():

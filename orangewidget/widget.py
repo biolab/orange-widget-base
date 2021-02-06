06193f03390b7741e44fe741e43ccfe21728f74d
Rafael Irgolic <hello@irgolic.com>
2021-02-06 19:23:07 +0000

    widget: Set correct buttonsArea margin

--- orangewidget/widget.py
+++ orangewidget/widget.py
@@ -499,7 +499,9 @@ class OWBaseWidget(QDialog, OWComponent, Report, ProgressBarMixin,
 
         if self.buttons_area_orientation is not None:
             self._insert_buttons_area()
-            self.buttonsArea.layout().setContentsMargins(4, 4, 0, 4)
+            self.buttonsArea.layout().setContentsMargins(*m)
+            # margins are nice on macOS with this
+            m = m[0], m[1], m[2], m[3] - 2
 
         self.controlArea.layout().setContentsMargins(*m)
 

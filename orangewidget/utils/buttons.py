eeb4f7d564d0b108d8259926369ed33c6a82d1ed
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2022-01-17 11:52:51 +0100

    buttons: Consistent hover background with MessagesWidget

--- orangewidget/utils/buttons.py
+++ orangewidget/utils/buttons.py
@@ -1,8 +1,8 @@
 from AnyQt.QtWidgets import (
-    QPushButton, QAbstractButton, QFocusFrame, QStyle, QStylePainter,
-    QStyleOptionButton
+    QPushButton, QAbstractButton, QStyle, QStylePainter, QStyleOptionButton,
+    QStyleOption
 )
-from AnyQt.QtGui import QPalette, QIcon
+from AnyQt.QtGui import QPalette, QIcon, QLinearGradient, QBrush, QPainter
 from AnyQt.QtCore import Qt, QSize, QEvent
 
 
@@ -69,29 +69,6 @@ class SimpleButton(QAbstractButton):
     """
     A simple icon button widget.
     """
-    def __init__(self, parent=None, **kwargs):
-        super().__init__(parent, **kwargs)
-        self.__focusframe = None
-
-    def focusInEvent(self, event):
-        # reimplemented
-        event.accept()
-        if self.__focusframe is None:
-            self.__focusframe = QFocusFrame(self)
-            self.__focusframe.setWidget(self)
-            palette = self.palette()
-            palette.setColor(QPalette.WindowText,
-                             palette.color(QPalette.Highlight))
-            self.__focusframe.setPalette(palette)
-
-    def focusOutEvent(self, event):
-        # reimplemented
-        event.accept()
-        if self.__focusframe is not None:
-            self.__focusframe.hide()
-            self.__focusframe.deleteLater()
-            self.__focusframe = None
-
     def event(self, event):
         if event.type() == QEvent.Enter or event.type() == QEvent.Leave:
             self.update()
@@ -104,25 +81,37 @@ class SimpleButton(QAbstractButton):
         icon = self.icon()
         if not icon.isNull():
             iconsize = icon.actualSize(iconsize)
-        return iconsize
+        margins = self.contentsMargins()
+        return QSize(
+            iconsize.width() + margins.left() + margins.right(),
+            iconsize.height() + margins.top() + margins.bottom()
+        )
 
     def minimumSizeHint(self):
         # reimplemented
         return self.sizeHint()
 
+    def initStyleOption(self, option: QStyleOptionButton):
+        option.initFrom(self)
+        option.features |= QStyleOptionButton.Flat
+        option.iconSize = self.iconSize()
+        option.icon = self.icon()
+        option.text = ""
+        if self.isDown():
+            option.state |= QStyle.State_Sunken
+
     def paintEvent(self, event):
         painter = QStylePainter(self)
         option = QStyleOptionButton()
-        option.initFrom(self)
-        option.text = ""
-        option.icon = self.icon()
-        option.iconSize = self.iconSize()
-        option.features = QStyleOptionButton.Flat
+        self.initStyleOption(option)
         if self.isDown():
-            option.state |= QStyle.State_Sunken
+            # Use default pressed flat button look
             painter.drawPrimitive(QStyle.PE_PanelButtonBevel, option)
+        elif option.state & (QStyle.State_MouseOver | QStyle.State_HasFocus):
+            flat_button_hover_background(painter, option)
 
         if not option.icon.isNull():
+            rect = option.rect.adjusted(1, 1, -1, -1)
             if option.state & QStyle.State_Active:
                 mode = (QIcon.Active if option.state & QStyle.State_MouseOver
                         else QIcon.Normal)
@@ -132,4 +121,46 @@ class SimpleButton(QAbstractButton):
                 state = QIcon.On
             else:
                 state = QIcon.Off
-            option.icon.paint(painter, option.rect, Qt.AlignCenter, mode, state)
+            option.icon.paint(painter, rect, Qt.AlignCenter, mode, state)
+
+
+def flat_button_hover_background(
+        painter: QPainter, option: QStyleOption
+):
+    palette = option.palette
+    g = QLinearGradient(0, 0, 0, 1)
+    g.setCoordinateMode(QLinearGradient.ObjectBoundingMode)
+    base = palette.color(QPalette.Window)
+    base.setAlpha(200)
+    if base.value() < 127:
+        base_ = base.lighter(170)
+    else:
+        base_ = base.darker(130)
+    g.setColorAt(0, base_)
+    g.setColorAt(0.6, base)
+    g.setColorAt(1.0, base_)
+    brush = QBrush(base_)
+    cg = palette_color_group(option.state)
+    if option.state & QStyle.State_HasFocus:
+        pen = palette.color(cg, QPalette.Highlight)
+    elif option.state & QStyle.State_MouseOver:
+        pen = palette.color(cg, QPalette.Foreground)
+        pen.setAlpha(50)
+    else:
+        pen = Qt.NoPen
+    painter.save()
+    painter.setPen(pen)
+    painter.setBrush(brush)
+    painter.translate(0.5, 0.5)
+    painter.setRenderHints(QPainter.Antialiasing, True)
+    painter.drawRoundedRect(option.rect.adjusted(0, 0, -1, -1), 2., 2., )
+    painter.restore()
+
+
+def palette_color_group(state: QStyle.StateFlag) -> QPalette.ColorGroup:
+    if not state & QStyle.State_Enabled:
+        return QPalette.Disabled
+    elif state & QStyle.State_Active:
+        return QPalette.Active
+    else:
+        return QPalette.Inactive

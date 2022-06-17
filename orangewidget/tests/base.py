ac0fb3b93123fcf0d4fed7f23b30a4ffab6456ec
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2022-06-17 11:27:10 +0200

    tests/base: Add GuiTest.tearDown

--- orangewidget/tests/base.py
+++ orangewidget/tests/base.py
@@ -196,6 +196,14 @@ class GuiTest(unittest.TestCase):
         super().tearDownClass()
         QTest.qWait(0)
 
+    def tearDown(self) -> None:
+        """
+        Process any pending events before the next test is executed. This
+        includes deletes scheduled with `QObject.deleteLater`.
+        """
+        super().tearDown()
+        QTest.qWait(0)
+
 
 class WidgetTest(GuiTest):
     """Base class for widget tests
@@ -274,7 +282,6 @@ class WidgetTest(GuiTest):
     def tearDown(self):
         """Process any pending events before the next test is executed."""
         self.signal_manager.clear()
-        QTest.qWait(0)
         super().tearDown()
 
     def create_widget(self, cls, stored_settings=None, reset_default_settings=True):

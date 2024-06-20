513f8940f8ae819991a28bef1ef9aee9ff76b816
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2024-06-20 23:19:52 +0200

    Numpy 2.0 compatibility
    Numpy 2.0 changes scalar repr as per:
    
         https://numpy.org/neps/nep-0051-scalar-representation.html
    
    Make matplotlib_export convert to python scalars where appropriate
    to avoid 'np.*' in generated code.

--- orangewidget/tests/test_matplotlib_export.py
+++ orangewidget/tests/test_matplotlib_export.py
@@ -1,7 +1,10 @@
+import numpy as np
 import pyqtgraph as pg
 
 from orangewidget.tests.base import GuiTest
-from orangewidget.utils.matplotlib_export import scatterplot_code
+from orangewidget.utils.matplotlib_export import (
+    scatterplot_code, numpy_repr, compress_if_all_same, numpy_repr_int
+)
 
 
 def add_intro(a):
@@ -15,8 +18,24 @@ class TestScatterPlot(GuiTest):
     def test_scatterplot_simple(self):
         plotWidget = pg.PlotWidget(background="w")
         scatterplot = pg.ScatterPlotItem()
-        scatterplot.setData(x=[1, 2, 3], y=[3, 2, 1])
+        scatterplot.setData(
+            x=np.array([1., 2, 3]),
+            y=np.array([3., 2, 1]),
+            size=np.array([1., 1, 1])
+        )
         plotWidget.addItem(scatterplot)
         code = scatterplot_code(scatterplot)
         self.assertIn("plt.scatter", code)
         exec(add_intro(code), {})
+
+    def test_utils(self):
+        a = np.array([1.5, 2.5])
+        self.assertIn("1.5, 2.5", numpy_repr(a))
+        a = np.array([1, 1])
+        v = compress_if_all_same(a)
+        self.assertEqual(v, 1)
+        self.assertEqual(repr(v), "1")
+        self.assertIs(type(v), int)
+        a = np.array([1, 2], dtype=int)
+        v = numpy_repr_int(a)
+        self.assertIn("1, 2", v)

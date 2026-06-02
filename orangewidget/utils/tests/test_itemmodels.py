ebb53c2861787e0a45fc8bf2b04df96f07867be9
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2026-06-02 14:24:19 +0200

    itemmodels: Extend tests

--- orangewidget/utils/tests/test_itemmodels.py
+++ orangewidget/utils/tests/test_itemmodels.py
@@ -660,8 +660,12 @@ class TestPyListModel(unittest.TestCase):
         model += [1, model.Separator]
         model.extend([1, model.Separator])
         for i in range(len(model)):
-            self.assertIs(model.flags(model.index(i)) == Qt.NoItemFlags,
-                          i % 2 != 0, f"in row {i}")
+            if i % 2:
+                midx = model.index(i)
+                self.assertIs(midx.data(Qt.DisplayRole), model.Separator)
+                self.assertEqual(midx.data(Qt.AccessibleDescriptionRole),
+                                 "separator")
+                self.assertEqual(midx.flags(), Qt.NoItemFlags)
 
     def test_separator_display_text(self):
         delegate = QStyledItemDelegate()
